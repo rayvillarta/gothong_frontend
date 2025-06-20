f8be7a69ccf2fcd4f293709395d04cc1211cbd48
jeff <alainnnsarmago@gmail.com>
2025-06-20 14:44:43 +0800

    feat: printable master manifest

--- static/ADMIN/view-master-manifest.js
+++ static/ADMIN/view-master-manifest.js
@@ -1,9 +1,3 @@
-// Add automatic print dialog if opened from the manifest list
-document.addEventListener("DOMContentLoaded", function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  if (urlParams.get("print") === "true") {
-    setTimeout(function () {
-      window.print();
-    }, 500);
-  }
+document.getElementById("printMasterManifest").addEventListener("click", () => {
+  window.print();
 });
